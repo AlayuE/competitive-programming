ece3f086c7b4191629ca2bbb575d2e26aa623c6f
AlayuE <61634503+AlayuE@users.noreply.github.com>
2023-02-03 22:47:41 +0300

    Time: 54 ms (56.56%), Space: 14.8 MB (46.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+
         left,right = 0,len(s)-1
         while left < right:
             while left < right and not s[left].isalnum():
@@ -12,4 +13,3 @@ class Solution:
             left += 1
             right -= 1
         return True
-        
