272fe5ed3b61fa4a3af7c6a1280f48c51be0d6f0
AlayuE <61634503+AlayuE@users.noreply.github.com>
2023-02-04 23:48:43 +0300

    Time: 623 ms (66.30%), Space: 35.5 MB (43.83%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        counter = 0
+        output = 0
+        sorted_arr = {}
+        for item in arr:
+            if item in sorted_arr:
+                sorted_arr[item] += 1
+            else:
+                sorted_arr[item] = 1
+        sorted_arr = sorted(sorted_arr.items(),key=lambda item: -item[1])
+        for k,v in sorted_arr:
+            counter += v # how many items are popped
+            output += 1
+            if counter >= (len(arr)//2):
+                break
+        return output
